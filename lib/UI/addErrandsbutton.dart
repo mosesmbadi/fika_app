dc3b7ea7e00dfd0338e52a0c6f03498875935084
Moses Mbadi <mosesmbadi@gmail.com>
2022-04-27 22:14:01 +0300

    designed UI of teh login Page

---
+++ lib/UI/addErrandsbutton.dart
@@ -0,0 +1,33 @@
+/*import 'package:flutter/material.dart';
+
+class addErrandsbutton extends StatefulWidget {
+  const addErrandsbutton({ Key? key }) : super(key: key);
+
+  @override
+  State<addErrandsbutton> createState() => _addErrandsbuttonState();
+}
+
+class _addErrandsbuttonState extends State<addErrandsbutton> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+            floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            Navigator.push (
+            context,
+            MaterialPageRoute (builder: (context)=> submitErrand()),
+            );
+            }, //addErrand
+          label: const Text('Add Errand'),
+          icon: const Icon(Icons.add),
+          backgroundColor: Colors.pink,
+        ),
+        ),
+      ),
+      
+      
+    );
+  }
+} */
--- lib/main.dart
+++ lib/main.dart
@@ -1,217 +1,49 @@
 import 'package:flutter/material.dart';
-import 'UI/submitErrand.dart';
 
-void main() {
-  // debugPaintSizeEnabled = true;
-  runApp(const MyApp());
-}
+import 'ui/login.dart';
+
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    Widget profileSection = Container(
-        padding: const EdgeInsets.all(10),
-        child: Row(
-          children: [
-            Container(
-              child: Text(
-                'Hi, Moses, welcome',
-                style: TextStyle(color: Colors.black),
-              ),
-            ),
-            Expanded(child: Container()),
-            CircleAvatar(
-              radius: 30.0,
-              backgroundImage: NetworkImage(
-                  'https://scontent.fmba5-1.fna.fbcdn.net/v/t39.30808-6/274512613_496889975138626_3466404655979511609_n.jpg?_nc_cat=105&ccb=1-5&_nc_sid=09cbfe&_nc_eui2=AeGiq9RpsaiiILS3MKaGpMlkr4oYMvkam-Kvihgy-Rqb4sx2sC5UrZsGc5ASV_zj1rzUGjgOydEsRf3rsArPIStp&_nc_ohc=6D2f1UScC5sAX89iwtR&_nc_pt=5&_nc_zt=23&_nc_ht=scontent.fmba5-1.fna&oh=00_AT-nQJmdYFclop8G9aFN9_y9sgYAtd_o7sEpknzF9FKX4w&oe=626AE74A'),
-              backgroundColor: Colors.amberAccent,
-            )
-          ],
-        ));
-
-    Widget daysSection = Container(
-      height: 100,
-      child: ListView(
-          scrollDirection: Axis.horizontal,
-          padding: const EdgeInsets.all(10),
-          children: [
-            Container(
-              height: 450,
-              width: 45,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blue,
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Center(
-                child: Column(
-                  children: [
-                    Text('Mon'),
-                    Text('10'),
-                  ],
-                ),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Container(
-              height: 450,
-              width: 45,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blue,
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Center(
-                child: Text('Tue'),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Container(
-              height: 450,
-              width: 45,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blue,
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Center(
-                child: Text('Wed'),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Container(
-              height: 450,
-              width: 45,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blue,
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Center(
-                child: Text('Thur'),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Container(
-              height: 450,
-              width: 45,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blue,
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Center(
-                child: Text('Fri'),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Container(
-              height: 450,
-              width: 45,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blue,
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Center(
-                child: Text('Sat'),
-              ),
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Container(
-              height: 450,
-              width: 45,
-              decoration: BoxDecoration(
-                border: Border.all(
-                  color: Colors.blue,
-                ),
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Center(
-                child: Text('Sun'),
-              ),
-            ),
-          ]),
-    );
-
-    Color color = Theme.of(context).primaryColor;
-
-    Widget textSection = const Padding(
-      padding: EdgeInsets.all(32),
-      child: Text(
-        'Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese '
-        'Alps. Situated 1,578 meters above sea level, it is one of the '
-        'larger Alpine Lakes. A gondola ride from Kandersteg, followed by a '
-        'half-hour walk through pastures and pine forest, leads you to the '
-        'lake, which warms to 20 degrees Celsius in the summer. Activities '
-        'enjoyed here include rowing, and riding the summer toboggan run.',
-        softWrap: true,
+    return new MaterialApp(
+      title: 'Fika',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
       ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
     );
+  }
+}
 
-    return MaterialApp(
-      title: 'Flutter layout demo',
-      home: Scaffold(
-        body: ListView(
-          children: [
-            profileSection,
-            daysSection,
-            textSection,
-          ],
-        ),
-        floatingActionButton: FloatingActionButton.extended(
-          onPressed: () {
-            Navigator.push (
-            context,
-            MaterialPageRoute (builder: (context)=> submitErrand(title: 'Add Errand Form',)),
-            );
-            }, //addErrand
-          label: const Text('Add Errand'),
-          icon: const Icon(Icons.add),
-          backgroundColor: Colors.pink,
-        ),
-      ),
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: LoginScreen(),
     );
   }
 
-  Column _buildButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
-            ),
+  Container buildPage(Color color, String text) {
+    return Container(
+      color: color,
+      child: Center(
+        child: Text(
+          text,
+          style: TextStyle(
+            fontSize: 50.0,
+            color: Colors.white,
           ),
         ),
-      ],
+      ),
     );
   }
 }
